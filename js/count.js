74f66aef9003e04678903b4522cb1474c420f65b
sumaiya006 <90856179+sumaiya006@users.noreply.github.com>
2022-01-14 19:46:50 +0600

    Add files via upload

---
+++ js/count.js
@@ -0,0 +1,26 @@
+$('.store').animationCounter({
+
+    start: 0,
+    step: 1,
+    end: 123,
+    delay: 10
+  
+  });
+
+  $('.menu').animationCounter({
+
+    start: 0,
+    step: 1,
+    end: 100,
+    delay: 12
+  
+  });
+
+  $('.chef').animationCounter({
+
+    start: 0,
+    step: 1,
+    end: 200,
+    delay: 6
+  
+  });
